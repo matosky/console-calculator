191430d7aa3b25c2b4d0892c738e03dff2b9a8b1
cyphercode <97557732+matosky@users.noreply.github.com>
2022-06-20 10:00:53 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,27 @@
+//addition calculator
+function add(num1,num2){
+  return num1 + num2;
+}
+
+//function subtract
+function subtract(num1,num2){
+  return num1-num2
+}
+
+//function multiply
+function multiply(num1,num2){
+  return num1*num2;
+}
+
+//function divide
+function divide(num1,num2){
+  return num1/num2;
+}
+
+//output
+
+console.log(add(55,55))
+console.log(subtract(77,333))
+console.log(multiply(66,33))
+console.log(divide(1,2))
+
