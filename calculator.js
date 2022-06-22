9e850fdb0389d4312e758d167d68f451f47248ba
cyphercode <97557732+matosky@users.noreply.github.com>
2022-06-22 12:52:51 -0700

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,27 +1,15 @@
-//addition calculator
-function add(num1,num2){
-  return num1 + num2;
+
+let num1 = parseInt(prompt("Enter number:"))
+let op= prompt("Enter operator:")
+
+let num2 = parseInt(prompt("Enter number:"))
+
+if(op=== "+"){
+  alert("Ans:" + (num1+num2))
+}else if(op==="-"){
+  alert("Ans:" + (num1-num2))
+}else if(op==="*"){
+  alert("Ans:" + (num1 * num2))
+}else if(op==="/"){
+  alert("Ans:" + (num1/num2))
 }
-
-//function subtract
-function subtract(num1,num2){
-  return num1-num2
-}
-
-//function multiply
-function multiply(num1,num2){
-  return num1*num2;
-}
-
-//function divide
-function divide(num1,num2){
-  return num1/num2;
-}
-
-//output
-
-console.log(add(55,55))
-console.log(subtract(77,333))
-console.log(multiply(66,33))
-console.log(divide(1,2))
-
